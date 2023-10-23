0a979d10a801efa7cc2bbd6c087e7b590722de8b
mayu0126 <ildikopinter0126@gmail.com>
2023-10-24 01:11:26 +0200

    Update main page, login and register

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-    <div>
-      SolarWatch Homepage - Sign In or Register to use the amazing SolarWatch app!
+    <div className="main-container">
+      <div className="main-message-1">
+        Get the sunrise and sunset data for your favourite cities
+      </div>
+      <div className="main-message-2">
+        - Sign In or Register to use the amazing SolarWatch app -
+      </div>
+      <div className="main-picture">
+      </div>
     </div>
     );
 }
-
-
 
 export default HomePage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
     //errorElement: <ErrorPage />,
     children: [
       {
-        path: "/solarwatch",
+        path: "/",
         element: <HomePage />,
       },
       {
